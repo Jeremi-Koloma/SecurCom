1fa35270f3de1c599aff6da63dcca00044f945e7
jkkoloma <kolomajeremi60@gmail.com>
2022-11-23 13:26:04 +0000

    Ajout de spring security pour l'authentification

--- src/main/java/com/GrafDigital/SecuCom/SecuCom/SecurityConfig.java
+++ src/main/java/com/GrafDigital/SecuCom/SecuCom/SecurityConfig.java
@@ -1,21 +1,52 @@
 package com.GrafDigital.SecuCom.SecuCom;
 
+import com.GrafDigital.SecuCom.SecuCom.Models.AppUser;
+import com.GrafDigital.SecuCom.SecuCom.Services.AccountService;
+import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import java.util.ArrayList;
+import java.util.Collection;
 
 // Cette classe va étendre de la classe WebSecurityConfigurerAdapter;
 @Configuration // une classe de configuration
 @EnableWebSecurity
+@AllArgsConstructor // Pour l'injections de service pour prendre la méthode qui va chargé le user
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-    // Définissons des méthodes;
+
+    // injectons notre service
+    private AccountService accountService;
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         // Ici on Idenfitie les Users qui ont le Droit d'accéder;
 
+        // Repérer le User avec ses droits grâce à la méthode loadUserByUserName;
+        auth.userDetailsService(new UserDetailsService() { // on le donne un Objet qui implement de l'interface UserDetailsService
+            @Override
+            public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+                // recupérer le User dans la base de donnée
+                AppUser appUser = accountService.loadUserByUserName(username);
+
+                Collection<GrantedAuthority> authorities = new ArrayList<>();
+                appUser.getAppRoles().forEach(r->{
+                    authorities.add(new SimpleGrantedAuthority(r.getRoleName()));
+                });
+
+                // On retourne un User de Spring et compare les informations
+                return new User(appUser.getUserName(), appUser.getPassword(), authorities);
+            }
+        });
 
     }
 
@@ -26,8 +57,17 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         // Désactivé le CSRF
         http.csrf().disable();
 
-        // Autorisé l'Accès à toutes les fonctionnalités;
-        http.authorizeRequests().anyRequest().permitAll(); // No Authentifaction any Request;
+        // Autorisé l'Accès aux fonctionnalités;
+
+        // Activé un formulaire d'authentification pour chaque User
+        http.formLogin();
+
+        // Toutes les requêtte doivent être authentifier;
+        http.authorizeRequests().anyRequest().authenticated();
+
+        //http.authorizeRequests().anyRequest().permitAll(); // No Authentifaction any Request;
+
+
 
     }
 }
